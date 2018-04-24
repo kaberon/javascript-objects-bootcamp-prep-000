02f98341a4955307fb9fcd66ed1dda5391fd8584
Ben Kaberon <kaberon@gmail.com>
2018-04-24 13:40:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {
   kanye: "wire",
+<<<<<<< HEAD
   jay: "bam",
 }
 
@@ -13,3 +14,15 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
+=======
+  jay: "bam"
+}
+
+function updatePlaylist(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+
+updatePlaylist (playlist, nas, "NYSOM")
+>>>>>>> b53a6f4e655789fedb53c00d43119c9224ad9d5f
